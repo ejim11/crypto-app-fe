f5fdf4df05e291cdad57c1626fe18f5045404be3
ejim11 <favourejim56@gmail.com>
2025-11-05 19:06:27 +0100

    ✨ added the home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,9 @@
+import 'package:crypto_app/widgets/home/account_setup_card.dart';
+import 'package:crypto_app/widgets/home/action_btns_section.dart';
+import 'package:crypto_app/widgets/home/balance_section.dart';
+import 'package:crypto_app/widgets/home/header.dart';
+import 'package:crypto_app/widgets/home/secondary_btns_section.dart';
+import 'package:crypto_app/widgets/home/transaction_history.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +11,34 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Header(),
+                    const SizedBox(height: 32),
+                    BalanceSection(),
+                    const SizedBox(height: 32),
+                    ActionBtnsSection(),
+                    const SizedBox(height: 24),
+                    SecondaryBtnsSection(),
+                    const SizedBox(height: 32),
+                    AccountSetupCard(),
+                    const SizedBox(height: 24),
+                    TransactionHistory(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
