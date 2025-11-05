35900577500d3a73ae9106eba4c0228340277710
ejim11 <favourejim56@gmail.com>
2025-11-05 19:03:59 +0100

    🎨 added the tab button list

---
+++ lib/data/tab_button_list.dart
@@ -0,0 +1,17 @@
+import 'package:crypto_app/models/tab_button_item.dart';
+
+List<TabButtonItem> tabButtonList = [
+  TabButtonItem(tabIcon: "assets/images/home/home-icon.png", tabLabel: "Home"),
+  TabButtonItem(
+    tabIcon: "assets/images/home/crypto-icon.png",
+    tabLabel: "Crypto",
+  ),
+  TabButtonItem(
+    tabIcon: "assets/images/home/gift-icon.png",
+    tabLabel: "Gift card",
+  ),
+  TabButtonItem(
+    tabIcon: "assets/images/home/transaction-icon.png",
+    tabLabel: "Transaction",
+  ),
+];
