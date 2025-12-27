cbb76b37dcd7e6e10cb4663b37eed05db38fd89d
ejim11 <favourejim56@gmail.com>
2025-12-27 08:02:19 +0100

    ✅ updated the home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -11,33 +11,36 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Header(),
-                    const SizedBox(height: 32),
-                    BalanceSection(),
-                    const SizedBox(height: 32),
-                    ActionBtnsSection(),
-                    const SizedBox(height: 24),
-                    SecondaryBtnsSection(),
-                    const SizedBox(height: 32),
-                    AccountSetupCard(),
-                    const SizedBox(height: 24),
-                    TransactionHistory(),
-                  ],
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(248, 251, 255, 1),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Header(headerText: 'Welcome, John'),
+                      const SizedBox(height: 32),
+                      BalanceSection(),
+                      const SizedBox(height: 32),
+                      ActionBtnsSection(),
+                      const SizedBox(height: 24),
+                      SecondaryBtnsSection(),
+                      const SizedBox(height: 32),
+                      AccountSetupCard(),
+                      const SizedBox(height: 24),
+                      TransactionHistory(),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
