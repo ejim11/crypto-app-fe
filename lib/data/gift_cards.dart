a6a07e5cd854b14f5d464a8176931740b883c346
ejim11 <favourejim56@gmail.com>
2025-12-27 07:59:21 +0100

    🗃️ added the gift card data

---
+++ lib/data/gift_cards.dart
@@ -0,0 +1,44 @@
+import 'package:crypto_app/models/gift_card_item.dart';
+
+final List<GiftCardItem> giftCards = [
+  GiftCardItem(
+    id: '1',
+    name: 'Google',
+    imageUrl: 'assets/images/gift_card/google_play.png',
+  ),
+  GiftCardItem(
+    id: '2',
+    name: 'Google',
+    imageUrl: 'assets/images/gift_card/google_play.png',
+  ),
+  GiftCardItem(
+    id: '3',
+    name: 'Google',
+    imageUrl: 'assets/images/gift_card/google_play.png',
+  ),
+  GiftCardItem(
+    id: '4',
+    name: 'Google',
+    imageUrl: 'assets/images/gift_card/google_play.png',
+  ),
+  GiftCardItem(
+    id: '5',
+    name: 'Google',
+    imageUrl: 'assets/images/gift_card/google_play.png',
+  ),
+  GiftCardItem(
+    id: '6',
+    name: 'Google',
+    imageUrl: 'assets/images/gift_card/google_play.png',
+  ),
+  GiftCardItem(
+    id: '7',
+    name: 'Amazon',
+    imageUrl: 'assets/images/gift_card/amazon.png',
+  ),
+  GiftCardItem(
+    id: '8',
+    name: 'iTunes',
+    imageUrl: 'assets/images/gift_card/itunes.png',
+  ),
+];
